16f472dd67f21eb50e1d1dd80f4b421fea183e3f
Echo <alexander.breadman@treecard.org>
2024-04-18 19:31:21 +0100

    fix

--- templates/js/pages/api/handcash/collection.js
+++ templates/js/pages/api/handcash/collection.js
@@ -5,30 +5,32 @@ dotenv.config();
 
 export default async(req, res) => {
 
-	console.log('creating collection')
-
 	const { authToken } = req.query;
 
-	var creds = { 
-		authToken: authToken,
-		appId: process.env.HANDCASH_APP_ID,
-		appSecret: process.env.HANDCASH_APP_SECRET,
-	}
-	
-	console.log(creds)
-
-	const handCashMinter = HandCashMinter.fromAppCredentials(creds);
-
-	const creationOrder = await handCashMinter.createCollection({
-		name: 'My first collection',
-		description: 'This is my first collection. Do not judge me.',
-		mediaDetails: {
-		  image: {
-			url: 'https://res.cloudinary.com/handcash-iae/image/upload/v1685141160/round-handcash-logo_cj47fp_xnteyo_oy3nbd.png',
-			contentType: 'image/png'
-		  }
+	const handCashMinter = HandCashMinter.fromAppCredentials(
+		{ 
+			"authToken": authToken,
+			"appId": process.env.HANDCASH_APP_ID,
+			"appSecret": process.env.HANDCASH_APP_SECRET,
 		}
-	});
+	);
 
-    res.status(200).json(creationOrder)
+	(async () => {
+		console.log('creating collection')
+
+		const creationOrder = await handCashMinter.createCollectionOrder({
+			name: 'HandCash Team Caricatures',
+			description: 'A unique collection of caricatures of the HandCash team',
+			mediaDetails: {
+				image: {
+					url: 'https://res.cloudinary.com/handcash-iae/image/upload/v1685141160/round-handcash-logo_cj47fp_xnteyo_oy3nbd.png',
+					contentType: 'image/png'
+				}
+			}
+		});
+		const items = await handCashMinter.getOrderItems(creationOrder.id);
+		console.log(`Collection Created, collectionId: ${items[0].id}`);
+	})();
+
+    res.status(200).json([])
 }
