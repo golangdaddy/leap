1d396a33e1352b1fef4b86e5ec1522914d97244b
Alexander Breadman <alexander.breadman@chase.io>
2024-09-03 22:17:23 +0100

    auto

--- templates/js/app/components/footer.js
+++ templates/js/app/components/footer.js
@@ -29,7 +29,7 @@ export default function Footer() {
 
 <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
     <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://{{.SiteName}}.com/" className="hover:underline">{{.SiteName}}</a>. All Rights Reserved.
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://{{.Config.SiteName}}.com/" className="hover:underline">{{.Config.SiteName}}</a>. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
