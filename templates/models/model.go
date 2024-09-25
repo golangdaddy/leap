f4bff3d6629cc76f8c1e244791413011b66ed900
Alexander Breadman <alexander.breadman@chase.io>
2024-09-25 09:24:18 +0100

    auto

--- templates/models/model.go
+++ templates/models/model.go
@@ -1,19 +1,19 @@
 {{range .Objects}}
 
-type {{uppercase .Name}} struct {
+type {{.ID}} struct {
 	Meta    Internals
-	Fields Fields{{uppercase .Name}} `json:"fields" firestore:"fields"`
+	Fields Fields{{.ID}} `json:"fields" firestore:"fields"`
 }
 
-func (user *User) New{{uppercase .Name}}(parent *Internals, fields Fields{{uppercase .Name}}) *{{uppercase .Name}} {
-	var object *{{uppercase .Name}}
+func (user *User) New{{.ID}}(parent *Internals, fields Fields{{.ID}}) *{{.ID}} {
+	var object *{{.ID}}
 	if parent == nil {
-		object = &{{uppercase .Name}}{
+		object = &{{.ID}}{
 			Meta: (Internals{}).NewInternals("{{lowercase .Name}}s"),
 			Fields: fields,
 		}
 	} else {
-		object = &{{uppercase .Name}}{
+		object = &{{.ID}}{
 			Meta: parent.NewInternals("{{lowercase .Name}}s"),
 			Fields: fields,
 		}
@@ -68,18 +68,18 @@ func (user *User) New{{uppercase .Name}}(parent *Internals, fields Fields{{upper
 	return object
 }
 
-type Fields{{uppercase .Name}} struct {
+type Fields{{.ID}} struct {
 	{{range .Fields}}{{.ID}} {{.Type}} `json:"{{.ID}}" firestore:"{{.ID}}"`
 	{{end}}
 }
 
-func (x *{{uppercase .Name}}) Schema() *models.Object {
+func (x *{{.ID}}) Schema() *models.Object {
 	obj := &models.Object{}
 	json.Unmarshal([]byte(`{{jsonmarshal .}}`), obj)
 	return obj
 }
 
-func (x *{{uppercase .Name}}) ValidateInput(w http.ResponseWriter, m map[string]interface{}) bool {
+func (x *{{.ID}}) ValidateInput(w http.ResponseWriter, m map[string]interface{}) bool {
 	if err := x.ValidateObject(m); err != nil {
 		cloudfunc.HttpError(w, err, http.StatusBadRequest)
 		return false
@@ -87,18 +87,18 @@ func (x *{{uppercase .Name}}) ValidateInput(w http.ResponseWriter, m map[string]
 	return true
 }
 
-func (x *{{uppercase .Name}}) ValidateObject(m map[string]interface{}) error {
+func (x *{{.ID}}) ValidateObject(m map[string]interface{}) error {
 
 	var err error
 	var exists bool
 	{{range .Fields}}
 
-	_, exists = m["{{lowercase .Name}}"]
+	_, exists = m["{{.ID}}"]
 	if {{.Required}} && !exists {
-		return errors.New("required field '{{lowercase .Name}}' not supplied")
+		return errors.New("required field '{{.ID}}' not supplied")
 	}
 	if exists {
-		x.Fields.{{titlecase .Name}}, err = assert{{tidy .Type}}(m, "{{lowercase .Name}}")
+		x.Fields.{{titlecase .Name}}, err = assert{{tidy .Type}}(m, "{{.ID}}")
 		if err != nil {
 			return errors.New(err.Error())
 		}
@@ -106,7 +106,7 @@ func (x *{{uppercase .Name}}) ValidateObject(m map[string]interface{}) error {
 			exp := "{{.Regexp}}"
 			if len(exp) > 0 {
 				if !RegExp(exp, fmt.Sprintf("%v", x.Fields.{{titlecase .Name}})) {
-					return fmt.Errorf("failed to regexp: %s >> %s", exp, x.Fields.{{titlecase .Name}})
+					return fmt.Errorf("failed to regexp: %s >> %s", exp, x.Fields.{{.ID}})
 				}
 			}
 		}
@@ -118,18 +118,18 @@ func (x *{{uppercase .Name}}) ValidateObject(m map[string]interface{}) error {
 				if err != nil {
 					log.Println(err)
 				}
-				if !RegExp(string(b), fmt.Sprintf("%v", x.Fields.{{titlecase .Name}})) {
-					return fmt.Errorf("failed to regexpHex: %s >> %s", string(b), x.Fields.{{titlecase .Name}})
+				if !RegExp(string(b), fmt.Sprintf("%v", x.Fields.{{.ID}})) {
+					return fmt.Errorf("failed to regexpHex: %s >> %s", string(b), x.Fields.{{.ID}})
 				}
 			}
 		}
 		{{if .Range}}
-		if err := assertRangeMin({{.Range.Min}}, x.Fields.{{titlecase .Name}}); err != nil {
+		if err := assertRangeMin({{.Range.Min}}, x.Fields.{{.ID}}); err != nil {
 			{{if .Required}}
 			return err
 			{{end}}
 		}
-		if err := assertRangeMax({{.Range.Max}}, x.Fields.{{titlecase .Name}}); err != nil {
+		if err := assertRangeMax({{.Range.Max}}, x.Fields.{{.ID}}); err != nil {
 			return err
 		}
 		{{end}}
@@ -154,7 +154,7 @@ func (x *{{uppercase .Name}}) ValidateObject(m map[string]interface{}) error {
 }
 
 // assert file is an image because of .Object.Options.Image
-func (object *{{uppercase .Name}}) ValidateImage{{uppercase .Name}}(fileBytes []byte) (image.Image, error) {
+func (object *{{.ID}}) ValidateImage{{.ID}}(fileBytes []byte) (image.Image, error) {
 
 	img, _, err := image.Decode(bytes.NewBuffer(fileBytes))
 	if err != nil {
